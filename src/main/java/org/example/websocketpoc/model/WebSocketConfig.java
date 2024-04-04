e1ea282ca09c0c794dbf5cca609cb39b84c958ae
蔣永寬 <mrzackchiang@gmail.com>
2024-04-04 22:54:15 +0800

    add allow origin zackawesome.net

--- src/main/java/org/example/websocketpoc/model/WebSocketConfig.java
+++ src/main/java/org/example/websocketpoc/model/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/endpointChatRoom").
-                setAllowedOrigins("mydomain.com").
+                setAllowedOrigins("zackawesome.net").
                 withSockJS();
         registry.addEndpoint("/chatroom-websocket");
     }
